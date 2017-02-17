51d00aa86346d160eb0ef836e6dfd841bfccd980
Greatbear <littlebeargreat@gmail.com>
2017-02-17 12:00:03 +0800

    路由，按需加载
    实现了控制器分离，多处view的更换，js脚本的引入

---
+++ 框架学习/angularJS/路由，按需加载/route/style/js/routeConfig/routeConfig.js
@@ -0,0 +1,63 @@
+var app = angular.module('myapp',['ui.router','oc.lazyLoad']);
+app.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRouterProvider) {
+	$urlRouterProvider.otherwise('/indexcont');
+	$stateProvider
+		.state('indexcont',{
+			url:'/indexcont',
+			views:{
+				"cont1":{
+					templateUrl:'html/indexcont.html'
+				}
+			}
+		})
+		.state('p1',{
+			url:'/p1',
+			views:{
+				"cont1":{
+					templateUrl:'html/p1.html'
+				},
+				'cont2':{
+					templateUrl:'html/p3.html'
+				}
+			},
+			resolve:{
+				loadMyCtrl:['$ocLazyLoad',function($ocLazyLoad){
+					return $ocLazyLoad.load('style/js/controller/p3.js');
+				}]
+			}
+		})
+		.state('p2',{
+			url:'/p2',
+			views:{
+				"cont1":{
+					templateUrl:'html/p2.html'
+				}
+			},
+			resolve:{
+				loadMyCtrl:['$ocLazyLoad',function($ocLazyLoad){
+					return $ocLazyLoad.load('style/js/controller/p2.js');
+				}]
+			}
+		})
+		.state('p3',{
+			url:'/p3',
+			views:{
+				"cont1":{
+					templateUrl:'html/p3.html'
+				}
+			},
+			resolve:{
+				loadMyCtrl:['$ocLazyLoad',function($ocLazyLoad){
+					return $ocLazyLoad.load('style/js/controller/p3.js');
+				}]
+			}
+		})
+		.state('p4',{
+			url:'/p4',
+			views:{
+				"cont1":{
+					templateUrl:'html/p4.html'
+				}
+			}
+		})
+}])
